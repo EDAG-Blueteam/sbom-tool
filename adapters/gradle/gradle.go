4342e3ce72df8187460e6fd3846050d91055be9c
Zulkhairi <mohd-zulkhairi.radzi@edag.com.my>
2022-12-19 01:20:48 +0800

    implement new structure for gradle

--- adapters/gradle/gradle.go
+++ adapters/gradle/gradle.go
@@ -1,8 +1,38 @@
 package gradle
 
+import (
+	"log"
+	"sbom-tool/structs"
+	"sbom-tool/utils"
+)
+
 type Gradle struct {
 }
 
 func (m *Gradle) Generate(file string) []byte {
 	return nil
+}
+
+func IsPackage(resultInfo structs.ResultInfo) bool {
+
+	var result bool = false
+
+	// TODO: validate gradle file
+
+	return result
+
+}
+
+func (m *Gradle) BuildToolsExist() bool {
+
+	output, err := utils.ExecCmd("gradle", "-v")
+
+	if err != nil {
+		log.Println("Cannot execute gradle")
+		return false
+	}
+
+	log.Println("The gradle version is : ", output)
+
+	return true
 }
