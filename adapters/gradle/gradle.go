901f2169697c08a42d0d40fb8019d871a86621d9
Zulkhairi <mohd-zulkhairi.radzi@edag.com.my>
2022-12-19 11:02:53 +0800

    Add sbom generation gradle command

--- adapters/gradle/gradle.go
+++ adapters/gradle/gradle.go
@@ -1,7 +1,9 @@
 package gradle
 
 import (
+	"io"
 	"log"
+	"os"
 	"sbom-tool/structs"
 	"sbom-tool/utils"
 )
@@ -10,6 +12,11 @@ type Gradle struct {
 }
 
 func (m *Gradle) Generate(file string) []byte {
+
+	InjectGradlePlugin()
+
+	utils.ExecCmd("gradle", "cyclonedxBom", "--build-file", "cyclonedxBuild.gradle")
+
 	return nil
 }
 
@@ -35,4 +42,28 @@ func (m *Gradle) BuildToolsExist() bool {
 	log.Println("The gradle version is : ", output)
 
 	return true
+}
+
+func InjectGradlePlugin() {
+	newGradleFile, err := os.Create("cyclonedxBuild.gradle")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer newGradleFile.Close()
+
+	gradleFile, err2 := os.Open("./build.gradle")
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	defer gradleFile.Close()
+
+	newGradleFile.WriteString("plugins {\n")
+	newGradleFile.WriteString("\tid 'org.cyclonedx.bom' version '1.7.2'\n")
+	newGradleFile.WriteString("}\n")
+
+	_, err3 := io.Copy(newGradleFile, gradleFile)
+	if err3 != nil {
+		log.Fatal(err3)
+	}
 }
